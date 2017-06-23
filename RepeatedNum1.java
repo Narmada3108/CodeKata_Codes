10b91f659e3cde440d72e886a62f50d9f808147e
Narmada3108 <narmada3196@gmail.com>
2017-06-23 14:19:07 +0530

    Create RepeatedNum1.java

---
+++ RepeatedNum1.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class RepeatedNum1 {
+	public static void main(String[] args)
+	{
+		int i,j;
+		System.out.println("size:");
+		Scanner s=new Scanner(System.in);
+		int s1=s.nextInt();
+		int a1 []=new int [s1];
+		for(i=0;i<s1;i++)
+		{
+			a1[i]=s.nextInt();
+		}
+		int v=0;
+		int a2[]=new int[30];
+		for(i=0;i<s1;i++)
+		{
+			for(j=i+1;j<s1;j++)
+			{
+				if(a1[i]==a1[j])
+				{
+					 
+				  a2[v]=a1[i];
+				  v++;
+				}
+			}	
+		 }
+		for(i=0;i<v;i++)
+		{
+			System.out.print(a2[i]+ " ");
+		}
+	}
+}
