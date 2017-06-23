173521d890534f03f10ac5c55ccf5276a5a9b65a
Narmada3108 <narmada3196@gmail.com>
2017-06-23 09:01:39 +0530

    Create Iso.java

---
+++ Iso.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class Iso {
+public static void main(String[] args)
+{
+	int flag=0,i,j,k;
+	Scanner s=new Scanner(System.in);
+	String s1=s.next();
+	String s2=s.next();
+	int l1=s1.length();
+	int l2=s2.length();
+	if (l1==l2)
+	{
+		for(i=0;i<l1;i++)
+		{
+			for(k=0;k<l1;k++)
+			{
+				if(s1.charAt(i)==s1.charAt(k))
+				{
+					if(s2.charAt(i)==s2.charAt(k))
+					{
+						break;
+					}
+					else
+						flag=1;
+				}
+			}
+		}
+		if(flag!=1)
+		{
+			System.out.print("They are Isometric Strings");
+		}
+		else
+		{
+			System.out.print("They are not Isometric Strings");
+		}
+	}
+	else
+	{
+		System.out.print("Input should be of equal length");
+	}
+	}
+}
